4851ecdd8e65ec06f55c373a476a3477d9c47370
xhyrom <generalkubo@gmail.com>
2022-07-01 18:11:27 +0200

    fix: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { readdir } from 'node:fs/promises';
 import { join } from 'path';
 const app = express();
+
+app.use(cors());
 
 (async() => {
     const apiFolder = (await readdir(join(__dirname, 'routes', 'api'))).filter(file => file.endsWith('.js'));
